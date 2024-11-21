b2240f5c66077e87128228c6439427947e66a753
MarcosHenriqueBarbosaPereira <marcos.h@gec.inatel.br>
2024-11-21 05:13:35 -0300

    fix: adjusted commentaries and default initial value for gpio_set_cfg function

--- components/gpio_control/include/gpio_control.h
+++ components/gpio_control/include/gpio_control.h
@@ -5,15 +5,15 @@
 #include "esp_err.h"
 
 typedef struct {
-	gpio_num_t pin;           // Número do pino GPIO
-	gpio_mode_t mode;         // Modo do GPIO (INPUT, OUTPUT, etc.)
-	gpio_pullup_t pullup;    // Resistores de pull-up
-	gpio_pulldown_t pulldown;  // Resistores de pull-down
+	gpio_num_t pin;           	// Número do pino GPIO
+	gpio_mode_t mode;         	// Modo do GPIO (INPUT, OUTPUT, etc.)
+	gpio_pullup_t pullup;    	// Resistores de pull-up
+	gpio_pulldown_t pulldown;  	// Resistores de pull-down
 } gpio_cfg_t;
 
 // Funções públicas
-esp_err_t gpio_set_cfg(gpio_cfg_t cfg, uint32_t initial_value);
-gpio_cfg_t *gpio_get_cfg(gpio_num_t pin); // Modificado para retornar por pino
+esp_err_t gpio_set_cfg(gpio_cfg_t cfg, uint32_t initial_value=0);
+gpio_cfg_t *gpio_get_cfg(gpio_num_t pin);
 esp_err_t gpio_write(gpio_num_t pin, uint32_t state);
 esp_err_t gpio_toggle(gpio_num_t pin);
 uint32_t gpio_read(gpio_num_t pin);
