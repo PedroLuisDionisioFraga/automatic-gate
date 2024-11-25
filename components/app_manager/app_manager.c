188e10c3151de2603a14f442224f5f20cb728df1
PedroLuisDionisioFraga <pedrodfraga@hotmail.com>
2024-11-24 22:59:26 -0300

    ✨ feat(gate): Add gate component with MQTT integration and task management
    This task will in progress and not complete, this commit is just to mark my progress

--- components/app_manager/app_manager.c
+++ components/app_manager/app_manager.c
@@ -16,12 +16,16 @@
 #include <esp_log.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <string.h>
 
+#include "gate.h"
 #include "mqtt5_secrets.h"
 #include "wifi_secrets.h"
 
 #define WIFI_CONNECTED_BIT BIT0
 #define MQTT_CONNECTED_BIT BIT1
+
+#define TOPIC_TO_FIRST_MESSAGE "first_message"
 
 #define FREERTOS_ERR_CHECK(x)                                       \
   do                                                                \
@@ -37,8 +41,10 @@ static const char *TAG = "APP MANAGER";
 
 static TaskHandle_t wifi_task_handle = NULL;
 static TaskHandle_t mqtt5_task_handle = NULL;
+static TaskHandle_t gate_task_handle = NULL;
 
 static EventGroupHandle_t wifi_connected_bit = NULL;
+static EventGroupHandle_t mqtt5_connected_bit = NULL;
 
 /**
  * @brief Manage the Wi-Fi connection.
@@ -65,14 +71,6 @@ static void wifi_task(void *pvParameters)
   }
 }
 
-void mqtt5_subscribe_callback(char *data, int len)
-{
-  ESP_LOGI(TAG, "Received message: %.*s", len, data);
-  ESP_LOGI(
-    TAG,
-    "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-}
-
 /**
  * @brief Manage the MQTT5 connection.
  *
@@ -81,7 +79,7 @@ void mqtt5_subscribe_callback(char *data, int len)
  * It runs indefinitely with a delay but does not perform any other actions.
  * In future versions, it may manage the MQTT5 connection.
  *
- * @param pvParameters Parameters passed to the task (not used).
+ * @param pvParameters Parameters passed to the task (not used).'
  */
 static void mqtt5_task(void *pvParameters)
 {
@@ -92,10 +90,43 @@ static void mqtt5_task(void *pvParameters)
                                          pdTRUE, pdFALSE, portMAX_DELAY);
   if (bits & WIFI_CONNECTED_BIT)
   {
-    ESP_LOGI(TAG, "Wi-Fi already connected, starting MQTT5...");
+    ESP_LOGI(TAG, "Wi-Fi connected, starting MQTT5...");
     mqtt5_api_start(MQTT5_URL, MQTT5_USERNAME, MQTT5_PASSWORD, MQTT5_PORT);
-    mqtt5_api_publish("c115/publishpedro", "Hello MQTT5", 11, 0, 0);
-    mqtt5_api_subscribe("c115/subscribe", 0, &mqtt5_subscribe_callback);
+
+    const char *msg = "MQTT5 connected!";
+
+    char topic[MAX_MQTT_TOPIC_LEN];
+    snprintf(topic, sizeof(topic), "%s/%s", BASE_MQTT_TOPIC,
+             TOPIC_TO_FIRST_MESSAGE);
+
+    esp_err_t ret;
+    ret = mqtt5_api_publish(topic, msg, strlen(msg));
+    if (ret != ESP_OK)
+      return;
+
+    xEventGroupSetBits(mqtt5_connected_bit, MQTT_CONNECTED_BIT);
+  }
+
+  while (1)
+  {
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+  }
+}
+
+static void gate_task(void *pvParameters)
+{
+  ESP_LOGI(TAG, "Starting gate task...");
+
+  // Wait for Wi-Fi to connect
+  EventBits_t bits = xEventGroupWaitBits(
+    mqtt5_connected_bit, MQTT_CONNECTED_BIT, pdTRUE, pdFALSE, portMAX_DELAY);
+
+  if (bits & MQTT_CONNECTED_BIT)
+  {
+    ESP_LOGI(TAG, "MQTT5 connected, starting gate...");
+
+    gate_t gate;
+    gate_init_impl(&gate, BASE_MQTT_TOPIC);
   }
 
   while (1)
@@ -109,12 +140,18 @@ void application_manager_init()
   wifi_connected_bit = xEventGroupCreate();
   xEventGroupClearBits(wifi_connected_bit, WIFI_CONNECTED_BIT);
 
+  mqtt5_connected_bit = xEventGroupCreate();
+  xEventGroupClearBits(mqtt5_connected_bit, MQTT_CONNECTED_BIT);
+
   ESP_LOGI(TAG, "Initializing Application Manager...");
   FREERTOS_ERR_CHECK(xTaskCreate(&wifi_task, "WiFi Task", 4096, NULL,
                                  tskIDLE_PRIORITY + 1, &wifi_task_handle));
 
   FREERTOS_ERR_CHECK(xTaskCreate(&mqtt5_task, "MQTT5 Task", 4096, NULL,
                                  tskIDLE_PRIORITY + 1, &mqtt5_task_handle));
+
+  FREERTOS_ERR_CHECK(xTaskCreate(&gate_task, "Gate Task", 4096, NULL,
+                                 tskIDLE_PRIORITY + 1, &gate_task_handle));
 
   ESP_LOGI(TAG, "Application Manager initialized.");
 }
